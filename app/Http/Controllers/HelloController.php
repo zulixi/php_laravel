79c5735d7fb28b06cd78009ff64027e982f8c78b
BuildTools <unconfigured@null.spigotmc.org>
2019-11-26 12:46:48 +0900

    バリデータを生成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -22,6 +23,15 @@ class HelloController extends Controller
     }
 
     public function post(HelloRequest $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|bewteen:0,150',
+        ]);
+
+        if($validator->fails()){
+            return redirect('/hello')->withErrors($validator)->withInput();
+        }
         return view('index', ['msg' => '正しく送信されました。']);
     }
 }
